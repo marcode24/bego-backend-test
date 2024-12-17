10f6db43da246de5c1ae56740f6c034ecd393fde
Marco Cruz <marco24cruz08@gmail.com>
2024-12-17 11:43:07 -0600

    🎉 Start project

---
+++ src/infraestructure/logging/Logger.ts
@@ -0,0 +1,35 @@
+import winston from 'winston';
+import path from 'path';
+
+const logLevels = {
+  levels: {
+    info: 0,
+    warn: 1,
+    error: 2,
+  },
+  colors: {
+    info: 'green',
+    warn: 'yellow',
+    error: 'red',
+  },
+};
+
+const __dirname = path.resolve();
+
+const logger = winston.createLogger({
+  levels: logLevels.levels,
+  transports: [
+    new winston.transports.File({
+      filename: path.join(__dirname, 'logs', 'app.log'),
+      level: 'info', // Solo guarda los logs de nivel 'info' o superior
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `[${timestamp}] ${level}: ${message}`;
+        })
+      ),
+    }),
+  ],
+});
+
+export default logger;
