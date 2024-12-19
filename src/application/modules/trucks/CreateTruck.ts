fae97c9029a1f2e0e182bc96c42256815bdd1696
Marco Cruz <marco24cruz08@gmail.com>
2024-12-19 02:28:03 -0600

    ✨ Enhance CreateTruck feature with detailed response messages and HTTP status codes

--- src/application/modules/trucks/CreateTruck.ts
+++ src/application/modules/trucks/CreateTruck.ts
@@ -4,6 +4,9 @@ import { CreateTruckRequest } from './DTOs/Requests/CreateTruckRequest.ts';
 import { CreateTruckResponse } from './DTOs/Responses/CreateTruckResponse.ts';
 import { Truck } from 'domain/entities/trucks/Truck.ts';
 import { IUserRepository } from 'domain/repositories/IUserRepository.ts';
+import { TruckMessages } from './messages/TruckMessages.ts';
+import { UserMessages } from '../users/messages/UserMessages.ts';
+import { HttpStatus } from 'infrastructure/common/HttpStatus.ts';
 
 @injectable()
 export default class CreateTruck {
@@ -15,16 +18,26 @@ export default class CreateTruck {
   public async execute(data: CreateTruckRequest): Promise<CreateTruckResponse<Truck | string>> {
     const userExists = await this.userRepository.findById(data.userId);
     if (!userExists) {
-      return CreateTruckResponse.UserNotFound();
+      return CreateTruckResponse.Failure(
+        UserMessages.UserNotFound(data.userId),
+        HttpStatus.NOT_FOUND
+      );
     }
 
     const truckExists = await this.truckRepository.findByPlate(data.plates);
     if (truckExists) {
-      return CreateTruckResponse.TruckAlreadyExists();
+      return CreateTruckResponse.Failure(
+        TruckMessages.TruckWithSamePlatesExists(data.plates),
+        HttpStatus.BAD_REQUEST
+      );
     }
 
     const truck = Truck.create(data.userId, data.year, data.color, data.plates);
     const newTruck = await this.truckRepository.create(truck);
-    return CreateTruckResponse.Success(newTruck);
+    return CreateTruckResponse.Success(
+      TruckMessages.CreateTruckSuccess,
+      HttpStatus.CREATED,
+      newTruck
+    );
   }
 }
