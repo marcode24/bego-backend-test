059daf267ccbfa9b5dafd1440d3d95a083733d8f
Marco Cruz <marco24cruz08@gmail.com>
2024-12-19 02:33:23 -0600

    ✨ Refactor dependency injection by separating truck and user container registrations

--- src/infrastructure/IoC/dependencyInjection.ts
+++ src/infrastructure/IoC/dependencyInjection.ts
@@ -1,34 +1,4 @@
-import { AuthController } from 'api/modules/auth/controllers/AuthController.ts';
-import { TruckController } from 'api/modules/trucks/controllers/TruckController.ts';
-import { UserController } from 'api/modules/users/controllers/UserController.ts';
-import LoginUser from 'application/modules/auth/LoginUser.ts';
-import CreateTruck from 'application/modules/trucks/CreateTruck.ts';
-import DeleteTruck from 'application/modules/trucks/DeleteTruck.ts';
-import GetTruck from 'application/modules/trucks/GetTruck.ts';
-import GetTrucks from 'application/modules/trucks/GetTrucks.ts';
-import UpdateTruck from 'application/modules/trucks/UpdateTruck.ts';
-import CreateUser from 'application/modules/users/CreateUser.ts';
-import { ITruckRepository } from 'domain/repositories/ITruckRepository.ts';
-import { IUserRepository } from 'domain/repositories/IUserRepository.ts';
-import { IJwtService } from 'domain/services/IJwtService.ts';
-import { TruckRepository } from 'infrastructure/Repositories/TruckRepository.ts';
-import { UserRepository } from 'infrastructure/Repositories/UserRepository.ts';
-import { JwtService } from 'infrastructure/services/JwtService.ts';
-import { container } from 'tsyringe';
+import 'infrastructure/IoC/truckContainer.ts';
+import 'infrastructure/IoC/userContainer.ts';
 
-container.registerSingleton<IUserRepository>('IUserRepository', UserRepository);
-container.registerSingleton<IJwtService>('IJwtService', JwtService);
-container.registerSingleton('CreateUser', CreateUser);
-container.registerSingleton('LoginUser', LoginUser);
-container.registerSingleton('UserController', UserController);
-container.registerSingleton('AuthController', AuthController);
-
-container.registerSingleton<ITruckRepository>('ITruckRepository', TruckRepository);
-container.registerSingleton('CreateTruck', CreateTruck);
-container.registerSingleton('GetTruck', GetTruck);
-container.registerSingleton('GetTrucks', GetTrucks);
-container.registerSingleton('DeleteTruck', DeleteTruck);
-container.registerSingleton('UpdateTruck', UpdateTruck);
-container.registerSingleton('TruckController', TruckController);
-
-export { container };
+export { container } from 'tsyringe';
