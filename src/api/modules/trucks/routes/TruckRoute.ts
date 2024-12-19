8160f2ca071e1158902f65c8c3a73c2222edadfa
Marco Cruz <marco24cruz08@gmail.com>
2024-12-19 17:53:15 -0600

    ✨ Add JWT validation middleware to Truck routes for enhanced security

--- src/api/modules/trucks/routes/TruckRoute.ts
+++ src/api/modules/trucks/routes/TruckRoute.ts
@@ -6,26 +6,28 @@ import { TruckController } from '../controllers/TruckController.ts';
 import { validateParams } from 'infrastructure/middlewares/validateParams.ts';
 import { truckIdValidator } from 'application/modules/trucks/validators/TruckIdValidator.ts';
 import { UpdateTruckValidator } from 'application/modules/trucks/validators/UpdateTruckValidator.ts';
+import { validateJwt } from 'infrastructure/middlewares/validateJwt.ts';
 
 export default (prefix: string, app: Router): void => {
   const truckController = container.resolve(TruckController);
 
-  app.get(`${prefix}/trucks`, (req, res) => truckController.findAll(req, res));
+  app.get(`${prefix}/trucks`, validateJwt, (req, res) => truckController.findAll(req, res));
 
-  app.get(`${prefix}/trucks/:id`, validateParams(truckIdValidator), (req, res) =>
+  app.get(`${prefix}/trucks/:id`, validateJwt, validateParams(truckIdValidator), (req, res) =>
     truckController.findById(req, res)
   );
 
-  app.post(`${prefix}/trucks`, validateRequest(CreateTruckValidator), (req, res) =>
+  app.post(`${prefix}/trucks`, validateJwt, validateRequest(CreateTruckValidator), (req, res) =>
     truckController.create(req, res)
   );
 
-  app.delete(`${prefix}/trucks/:id`, validateParams(truckIdValidator), (req, res) =>
+  app.delete(`${prefix}/trucks/:id`, validateJwt, validateParams(truckIdValidator), (req, res) =>
     truckController.delete(req, res)
   );
 
   app.put(
     `${prefix}/trucks/:id`,
+    validateJwt,
     validateParams(truckIdValidator),
     validateRequest(UpdateTruckValidator),
     (req, res) => truckController.update(req, res)
